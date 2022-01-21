585c5c00a4cb7e71d3be385dc6a750df9ec76ef1
menta <jimementa@gmail.com>
2022-01-21 20:51:01 +0100

    falta logica en botones

---
+++ src/app/scene/scene.component.ts
@@ -0,0 +1,41 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-scene',
+  templateUrl: './scene.component.html',
+  styleUrls: ['./scene.component.css']
+})
+export class SceneComponent implements OnInit {
+  @Input() story:string[];
+ 
+  currentSentence:string;
+  counter=0;
+  
+  e:Event
+  next(event:Event){
+    if(this.counter <= 0){
+      this.counter++;
+    }
+    this.currentSentence = this.story[this.counter];
+   
+  }
+ 
+  prev(event:Event){
+    if(this.counter > 0){
+      this.counter--;
+    }  
+    this.currentSentence = this.story[this.counter];
+  }
+  
+
+
+
+  constructor() { 
+   
+  }
+
+  ngOnInit(): void {
+    this.currentSentence= this.story[0];
+  }
+
+}
